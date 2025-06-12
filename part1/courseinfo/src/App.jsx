07a79ca91ed2b492faa8e68a1947ed45d96c00c5
Onur <onur.onel2230@gmail.com>
2025-06-11 21:18:41 -0400

    Updates

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,64 +1,51 @@
-const Header = (props) => {
-  console.log(props)
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
+const CourseParts = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
 
-const Content = (props) => {
-  console.log(props)
-  return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
-  )
-}
+const MapCourse = ({ parts }) => (
+  <>
+    {parts.map((part) => (
+      <CourseParts key={part.id} name={part.name} exercises={part.exercises} />
+    ))}
+  </>
+);
 
-
-
-const Total = (props) => {
-  console.log(props)
-
-  return (
-    <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
-  )
-}
-
-
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Total of {total} exercises</p>;
+};
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    id: 1,
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content part={course.parts[0].name} exercises={course.parts[0].exercises} />
-      <Content part={course.parts[1].name} exercises={course.parts[1].exercises} />
-      <Content part={course.parts[2].name} exercises={course.parts[2].exercises} />
-      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises} />
-    </div>
-  )
-}
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
 
-export default App
+  return (
+    <>
+      <MapCourse parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+export default App;
