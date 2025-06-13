f264945d9e2a97b776c440af137380d1e2241ff1
Onur <onur.onel2230@gmail.com>
2025-06-12 23:13:42 -0400

    Updates

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,51 @@
 import { useState } from "react";
 
 const PersonItem = ({ person }) => {
-  return <li>{person.name}</li>;
+  return (
+    <li>
+      {person.name} {person.number}
+    </li>
+  );
 };
 
 const App = () => {
-  const [contactList, setContactList] = useState([{ name: "Arto Hellas" }]);
+  const [contactList, setContactList] = useState([
+    { name: "Onur Onel", number: "+1 613 853 1334" },
+  ]);
   const [inputName, setInputName] = useState("");
+  const [inputNumber, setInputNumber] = useState("");
 
   const handleAddContact = (event) => {
     event.preventDefault();
     const newContact = {
       name: inputName,
+      number: inputNumber,
     };
+    const isNameExists = contactList.some(
+      (person) => person.name === inputName
+    );
+    const isNumberExists = contactList.some(
+      (person) => person.number === inputNumber
+    );
+
+    if (isNameExists || isNumberExists) {
+      let message = "";
+      if (isNameExists) message += `Name "${inputName}" already exists.\n`;
+      if (isNumberExists) message += `Number "${inputNumber}" already exists.`;
+      return alert(message.trim());
+    }
+
     setContactList(contactList.concat(newContact));
     setInputName("");
+    setInputNumber("");
   };
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     setInputName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setInputNumber(event.target.value);
   };
 
   return (
@@ -26,8 +53,17 @@ const App = () => {
       <h1>Phonebook</h1>
 
       <form onSubmit={handleAddContact}>
-        Name <input value={inputName} onChange={handleInputChange} />
-        <button>add</button>
+        <div>
+          Name <input value={inputName} onChange={handleNameChange} />
+        </div>
+        <br />
+        <div>
+          Number: <input value={inputNumber} onChange={handleNumberChange} />
+        </div>
+        <br />
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
 
       <h2>Contacts</h2>
