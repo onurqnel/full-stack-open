e3761c7fbebeffafe65cac647c419ea61ee11e33
Onur <onur.onel2230@gmail.com>
2025-06-24 22:59:34 -0400

    Updates

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1
+      id: notes.length + 1,
     };
     setNotes(notes.concat(noteObject));
     setNewNote("");
@@ -22,7 +33,9 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important === true);
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
 
   return (
     <div>
--- part2/notes/src/main.jsx
+++ part2/notes/src/main.jsx
@@ -1,15 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import axios from "axios";
 
-axios
-  .get("http://localhost:3001/notes")
-  .then((response) => {
-    const notes = response.data;
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <App notes={notes} />
-    );
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
