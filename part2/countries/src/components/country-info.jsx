a121f86e00078df8e5af5444fe840ef2500f447f
Onur <onur.onel2230@gmail.com>
2025-06-29 18:13:08 -0400

    Updates

--- part2/countries/src/components/country-info.jsx
+++ part2/countries/src/components/country-info.jsx
@@ -13,6 +13,12 @@ const CountryInfo = ({ country }) => {
       <p>
         <strong>Population:</strong> {country.population.toLocaleString()}
       </p>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(country.languages).map((lang, index) => (
+          <li key={index}>{lang}</li>
+        ))}
+      </ul>
       <img
         src={country.flags?.png}
         alt={`Flag of ${country.name.common}`}
