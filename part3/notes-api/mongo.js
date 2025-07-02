e8df4dca363bdfddcea5974b0255242c8d563b6f
Onur <onur.onel2230@gmail.com>
2025-07-01 22:09:45 -0400

    Updates

---
+++ part3/notes-api/mongo.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const username = process.env.MONGODB_USERNAME;
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const url = `mongodb+srv://${username}:${password}@cluster0.4smbkro.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+console.log("Şifre:", process.env.MONGODB_PASSWORD);
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
