15c9c9ad9934ca6932da966d3d3630abd64381ab
Onur <onur.onel2230@gmail.com>
2025-07-04 17:38:12 -0400

    Updates

--- part3/notes-api/index.js
+++ part3/notes-api/index.js
@@ -18,6 +18,28 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
